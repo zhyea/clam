08f22b5ab4c6f67eb9a57134f66f60aa585df4d3
zhyea <zhangrui@zhyea.com>
2024-07-26 22:40:23 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="content">
+		<view>
+			<image src="../../static/clam.gif" />
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '这是我的第一个小程序'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
